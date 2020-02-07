f1670a1e068711d1288fed7b31ad76fedd49f387
karina-hensel <henselkarina@gmail.com>
2020-02-07 22:02:25 +0100

    Trigrams

--- Preprocessing/Preprocessing.py
+++ Preprocessing/Preprocessing.py
@@ -1,4 +1,5 @@
 '''Neural network to predict the next word in a sequence based on the previous token'''
+from keras_preprocessing.sequence import pad_sequences
 from nltk.corpus import gutenberg, stopwords
 from nltk.tokenize import word_tokenize
 import numpy as np
@@ -40,7 +41,7 @@ def extract_word_vectors(corpus):
 
 def extract_word_vectors2(corpus, num_sent):
     '''Use bigrams as input (not completely working at the moment)'''
-    sents = gutenberg.sents('austen-emma.txt')[:num_sent]
+    sents = gutenberg.sents(corpus)[:num_sent]
     text = ''
 
     for s in sents:
@@ -74,15 +75,14 @@ def extract_word_vectors2(corpus, num_sent):
     return [X, y, vocab_len, tokenizer]
 
 
-def extract_bigram_vectors(corpus, num_sent):
+def extract_trigram_vectors(corpus, num_sent):
     '''Read in the specified number of sentences from the corpus'''
-    sents = gutenberg.sents('austen-emma.txt')[:num_sent]
+    sents = gutenberg.sents(corpus)[:num_sent]
+    #text = gutenberg.raw(corpus)[:num_sent]
     text = ''
-
     for s in sents:
         for w in s:
             text += ' ' + w
-
     # Extract one word and the following one
     tokenizer = Tokenizer()
     # Extracts sequences of text
@@ -94,10 +94,15 @@ def extract_bigram_vectors(corpus, num_sent):
     vocab_len = len(tokenizer.word_index) + 1
 
     sequences = list()
-    # Create bi-gram - one-gram sequences
+
+    # Create 3 word sequences
     for i in range(2, len(int_enc)):
         tmp = int_enc[i - 2:i + 1]
         sequences.append(tmp)
+
+    # Find padding length
+    max_len = max([len(s) for s in sequences])
+    sequences = pad_sequences(sequences, maxlen=max_len, padding='pre')
 
     sequences2 = array(sequences)
     X = sequences2[:, :-1]
@@ -106,13 +111,13 @@ def extract_bigram_vectors(corpus, num_sent):
     # Use Keras to_categorical() function to one-hot encode the output / second word
     y = to_categorical(y, num_classes=vocab_len)
 
-    return [X, y, vocab_len, tokenizer]
+    return [X, y, vocab_len, tokenizer, max_len]
 
-def model(X, y, vocab_len):
+def model(X, y, vocab_len, input_len):
     # Create the actual model
     # Embedding layer to learn the word embeddings from the input; input_length=1 because 1 word at a time is passed to NN
     model = Sequential()
-    model.add(Embedding(vocab_len, 10, input_length=1))
+    model.add(Embedding(vocab_len, 10, input_length=input_len-1))
     model.add(LSTM(50))
     model.add(Dense(vocab_len, activation='softmax'))
     model.summary()
@@ -125,32 +130,39 @@ def model(X, y, vocab_len):
     return model
 
 # Try to generate an entire sentence
-def gen_sent(start, model, tokenizer):
-    start_vec = tokenizer.texts_to_sequences([start.lower()])[0]
-    start_vec = array(start_vec)
-    sentence = [start]
-    pred = model.predict_classes(start_vec, verbose=0)
-    c = 0
-    while c < 10:
+def gen_sent(start, max_len, model, tokenizer):
+    '''Generate sequences of ten tokens starting with the given input text'''
+    text = start
+    sentence = [text]
+    # Predict the next word until a sequence of input_len + 10 words is generated
+    for c in  range(11):
+        # Retrieve vector of current word
+        vec = tokenizer.texts_to_sequences([text])[0]
+        # Add padding
+        vec = pad_sequences([vec], maxlen=max_len, padding='pre')
+        # Predict next word
+        pred = model.predict_classes(vec, verbose=0)
+
+        # Look up the word of the predicted index and append it to the sequence
         next_w = ''
         for ind, wrd in tokenizer.index_word.items():
             if ind == pred:
                 sentence.append(wrd)
                 next_w = wrd
-            if c > 10:
                 break
-        vec = tokenizer.texts_to_sequences([next_w])[0]
-        vec = array(vec)
-        pred = model.predict_classes(vec, verbose=0)
-        c += 1
+        text += ' ' + next_w
+
     return sentence
 
 # Test
-word_vec = extract_word_vectors2('austen-emma.txt', 500)
+#word_vec = extract_word_vectors2('austen-emma.txt', 500)
+word_vec = extract_trigram_vectors('austen-emma.txt', 500)
+
 X = word_vec[0]
 y = word_vec[1]
 vocab_len = word_vec[2]
 tokenizer = word_vec[3]
-model = model(X, y, vocab_len)
+max_len = word_vec[4]
+model = model(X, y, vocab_len, max_len)
 
-print(gen_sent('emma', model, tokenizer))
+print(gen_sent('emma has', max_len-1, model, tokenizer))
